cea66bb214f5911eb3fd3c5dc76205dee652654c
Letsoken <lettyisoken@gmail.com>
2023-01-31 22:24:00 +0100

    Correct task 3

--- 0x08-python-more_classes/3-rectangle.py
+++ 0x08-python-more_classes/3-rectangle.py
@@ -60,7 +60,7 @@ class Rectangle:
             The area of rectangle
         """
 
-        return (self.__width * self.__height)
+        return self.__width * self.__height
 
     def perimeter(self):
         """Calculates perimeter of rectangle
@@ -69,10 +69,10 @@ class Rectangle:
             The rectangle perimeter
         """
 
-        if (self.__width or self.__height) == 0:
+        if self.__width == 0 or self.__height == 0:
             return (0)
         else:
-            return (2 * (self.__width + self.__height))
+            return 2 * (self.__width + self.__height)
 
     def __str__(self):
         """Returns an “informal” and nicely printable
