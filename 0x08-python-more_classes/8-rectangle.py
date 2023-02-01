2b5f5fe63ef85023f418dc2e502b5736c73ae5ef
Letsoken <lettyisoken@gmail.com>
2023-02-01 05:29:25 +0100

    t08

--- 0x08-python-more_classes/8-rectangle.py
+++ 0x08-python-more_classes/8-rectangle.py
@@ -117,7 +117,7 @@ class Rectangle:
             rect_2: An instance of a rectangle
 
         Returns:
-        The biggest rectangle based on the area
+            The biggest rectangle based on the area
         """
 
         if not isinstance(rect_1, Rectangle):
@@ -125,7 +125,7 @@ class Rectangle:
 
         if not isinstance(rect_2, Rectangle):
             raise TypeError("rect_2 must be an instance of Rectangle")
-        if rect_1.area() >= rect_2.area():
+        if rect_1.area() == rect_2.area() or rect_1.area() > rect_2.area():
             return rect_1:
         else:
             return rect_2
