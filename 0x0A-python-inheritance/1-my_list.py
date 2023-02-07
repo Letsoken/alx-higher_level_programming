4251b2fbab124325748f7eaeb97f65314492aa05
Letsoken <lettyisoken@gmail.com>
2023-02-07 03:43:15 +0100

    01

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""Task 1
+"""
+
+
+class Mylist(list):
+    """A subclass of the list class"""
+
+    def print_sorted(self):
+        """Function that prints the list, but sorted (ascending sort)"""
+        print(self.(sort))
